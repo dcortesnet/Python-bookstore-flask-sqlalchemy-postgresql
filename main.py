75d3512d59acac6e05d89902d92665038328cc6f
dcortesnet <dcortes.net@gmail.com>
2024-01-14 22:18:51 -0300

    Add models and main app

--- main.py
+++ main.py
@@ -1,16 +1,96 @@
-from flask import Flask
-from models import db
+from flask import Flask, request, jsonify
+from models import db, Author, Book
 
 app = Flask(__name__)
 port = 5000
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://postgres:@localhost:5432/test'
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://postgres:123456@localhost:5432/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
+@app.route('/authors', methods=['GET'])
+def get_authors():
+    try:
+        authors = Author.query.all()
+        authors_data = []
+        for author in authors:
+            author_data = {
+                'id': author.id,
+                'name': author.name,
+                'age': author.age,
+                'books': []
+            }
+            for book in author.books:
+                book_data = {
+                    'id': book.id,
+                    'isbn': book.isbn,
+                    'name': book.name,
+                    'cant_pages': book.cant_pages,
+                    'createdAt': book.created_at
+                }
+                author_data['books'].append(book_data)
+            authors_data.append(author_data)
+        return jsonify({'authors': authors_data})
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+@app.route('/authors', methods=['POST'])
+def add_author():
+    try:
+        data = request.json
+        name = data.get('name')
+        age = data.get('age')
+        if not name or not age:
+            return jsonify({'message': 'Bad request, name or age not found'}), 400
+        new_author = Author(name=name, age=age)
+        db.session.add(new_author)
+        db.session.commit()
+        return jsonify({'author': {'id': new_author.id, 'name': new_author.name, 'age': new_author.age}}), 201
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    try:
+        books = Book.query.all()
+        books_data = []
+        for book in books:
+            book_data = {
+                'id': book.id,
+                'isbn': book.isbn,
+                'name': book.name,
+                'cant_pages': book.cant_pages
+            }
+            books_data.append(book_data)
+        return jsonify({'books': books_data})
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    try:
+        data = request.json
+        isbn = data.get('isbn')
+        name = data.get('name')
+        cant_pages = data.get('cant_pages')
+        author_id = data.get('author')
+        if not name or not cant_pages or not author_id or not isbn:
+            return jsonify({'message': 'Bad request, isbn or name or cantPages or author not found'}), 400
+        new_book = Book(isbn=isbn, name=name, cant_pages=cant_pages, author_id=author_id)
+        db.session.add(new_book)
+        db.session.commit()
+        return jsonify({'book': {'id': new_book.id, 'isbn': new_book.isbn, 'name': new_book.name, 'cant_pages': new_book.cant_pages}}), 201
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=port)
     db.init_app(app)
-    print('Server listen on http://localhost:{port}'.format(port))
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', debug=True, port=port)
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from flask_sqlalchemy import SQLAlchemy
 import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
@@ -8,7 +8,7 @@ class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.datetime)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
     books = db.relationship("Book")
 
 class Book(db.Model):
@@ -17,5 +17,6 @@ class Book(db.Model):
     isbn = db.Column(db.String(255), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     cant_pages = db.Column(db.Integer, nullable=False, default=0)
-    created_at = db.Column(db.DateTime, default=datetime.datetime)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
     author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+
